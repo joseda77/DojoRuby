8879dcdc2cf697d0683b23befcb8a678c0074a1d
Cesar Muñoz <cesar.munoz@ceiba.com.co>
2018-09-08 10:16:38 -0500

    Dojo ruby completo

---
+++ db/migrate/20180908140519_create_applies.rb
@@ -0,0 +1,11 @@
+class CreateApplies < ActiveRecord::Migration[5.1]
+  def change
+    create_table :applies do |t|
+      t.string :title
+      t.text :body
+      t.string :name
+      t.integer :age
+      t.timestamps
+    end
+  end
+end
